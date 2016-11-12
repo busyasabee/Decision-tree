ad69c1646a2ce896cfe8df58666c64e785c9b191
Дмитрий <@DMITRIY>
2016-11-12 16:47:49 +0400

    End of attribute problem

--- Desicion tree/Tree.cs
+++ Desicion tree/Tree.cs
@@ -29,6 +29,7 @@ namespace Desicion_tree
         string[] tableHeader;
         bool firstTime = true;
         //string[,] table = new string[9, 5] {"Соперник", "Играем","Лидеры" };
+
         public void readFile(string path)
         {
             string line;
@@ -42,7 +43,7 @@ namespace Desicion_tree
                 tableList.Add(str);
 
             }
-            usedAttributes = new bool[colsCount - 2];
+            usedAttributes = new bool[colsCount];
             tableHeader = tableList[0];
             tableList.RemoveAt(0);
             lineCount = tableList.Count;
@@ -78,6 +79,8 @@ namespace Desicion_tree
             int countWinYes = 0;
             int countWinNo = 0;
             int lineCount = lineNumbers.Count;
+            List<int> firstValueLineNumbers = new List<int>();
+            List<int> secondValueLineNumbers = new List<int>();
 
             int colNumber = Array.IndexOf(tableHeader, "Победа");
             for (int i = 0; i < lineNumbers.Count; i++)
@@ -90,67 +93,142 @@ namespace Desicion_tree
             }
             double pNo = (double)countWinNo / lineCount;
             double pYes = (double)countWinYes / lineCount;
+            if (pNo == 0)
+            {
+                pNo = 1;
+            }
+            if (pYes==0)
+            {
+                pYes = 1;
+            }
             entropyT = -pNo * Math.Log(pNo, 2) - pYes * Math.Log(pYes, 2);
-            Console.WriteLine("Энтропия вершины " + entropyT);
+            Console.Write("Энтропия вершины ");
+            for (int i = 0; i < lineNumbers.Count; i++)
+            {
+                Console.Write(lineNumbers[i] + " ");
+            }
+            Console.WriteLine("равна " + entropyT);
+            if (entropyT == 0) return; //выходим
             //List<double> childEntropyList = new List<double>(); //надо ещё запоминать номера столбцов
             Dictionary<int, double> childEntropyDict = new Dictionary<int, double>();
             // сделать перебор атрибутов
             for (int i = 1; i < colsCount-1; i++)
             {
-                double firstValueCount, secondValueCount;
-                firstValueCount = secondValueCount = 0d;
-                double firstValueYesCount, firstValueNoCount;
-                firstValueYesCount = firstValueNoCount = 0d;
-                double secondValueYesCount, secondValueNoCount;
-                secondValueYesCount = secondValueNoCount = 0d;
-                List<int> firstValueLineNumbers = new List<int>();
-                List<int> secondValueLineNumbers = new List<int>();
-                for (int j = 0; j < lineNumbers.Count; j++)
+                if (usedAttributes[i] != true)
                 {
-                    if (tableList[lineNumbers[j]][i] == attributeValuesDict[i][0])
+                    double firstValueCount, secondValueCount;
+                    firstValueCount = secondValueCount = 0d;
+                    double firstValueYesCount, firstValueNoCount;
+                    firstValueYesCount = firstValueNoCount = 0d;
+                    double secondValueYesCount, secondValueNoCount;
+                    secondValueYesCount = secondValueNoCount = 0d;
+                    //List<int> firstValueLineNumbers = new List<int>();
+                    //List<int> secondValueLineNumbers = new List<int>();
+                    for (int j = 0; j < lineNumbers.Count; j++)
                     {
-                        ++firstValueCount;
-                        firstValueLineNumbers.Add(lineNumbers[j]);
+                        if (tableList[lineNumbers[j]][i] == attributeValuesDict[i][0])
+                        {
+                            ++firstValueCount;
+                            firstValueLineNumbers.Add(lineNumbers[j]);
+                        }
+                        else
+                        {
+                            ++secondValueCount;
+                            secondValueLineNumbers.Add(lineNumbers[j]);
+                        }
                     }
-                    else
+                    double p1 = firstValueCount / lineCount;
+                    double p2 = secondValueCount / lineCount;
+                    double p1Yes, p1No, p2Yes, p2No;
+                    for (int j = 0; j < firstValueLineNumbers.Count; j++)
                     {
-                        ++secondValueCount;
-                        secondValueLineNumbers.Add(lineNumbers[j]);
+                        if (tableList[firstValueLineNumbers[j]][colNumber] == "да")
+                        {
+                            firstValueYesCount += 1;
+                        }
+                        else firstValueNoCount += 1;
                     }
-                }
-                double p1 = firstValueCount / lineCount;
-                double p2 = secondValueCount / lineCount;
-                double p1Yes, p1No, p2Yes, p2No;
-                for (int j = 0; j < firstValueLineNumbers.Count; j++)
-                {
-                    if (tableList[firstValueLineNumbers[j]][colNumber] == "yes")
+                    p1Yes = firstValueYesCount / firstValueLineNumbers.Count;
+                    // чтобы не брать логорифм от 0
+                    if (p1Yes==0)
                     {
-                        firstValueYesCount += 1;
+                        p1Yes = 1;
                     }
-                    else firstValueNoCount += 1; 
-                }
-                p1Yes = firstValueYesCount / firstValueLineNumbers.Count;
-                p1No = firstValueNoCount / firstValueLineNumbers.Count;
+                    p1No = firstValueNoCount / firstValueLineNumbers.Count;
+                    if (p1No == 0)
+                    {
+                        p1No = 1;
+                    }
 
-                for (int j = 0; j < secondValueLineNumbers.Count; j++)
-                {
-                    if (tableList[secondValueLineNumbers[j]][colNumber] == "yes")
+                    for (int j = 0; j < secondValueLineNumbers.Count; j++)
                     {
-                        secondValueYesCount += 1;
+                        if (tableList[secondValueLineNumbers[j]][colNumber] == "да")
+                        {
+                            secondValueYesCount += 1;
+                        }
+                        else secondValueNoCount += 1;
                     }
-                    else secondValueNoCount += 1;
+                    p2Yes = secondValueYesCount / secondValueLineNumbers.Count;
+                    if (p2Yes == 0)
+                    {
+                        p2Yes = 1;
+                    }
+                    p2No = secondValueNoCount / secondValueLineNumbers.Count;
+                    if (p2No == 0)
+                    {
+                        p2No = 1;
+                    }
+                    entropyChild = p1 * (-p1Yes * Math.Log(p1Yes, 2) - p1No * Math.Log(p1No, 2)) + p2 * (-p2Yes * Math.Log(p2Yes, 2) - p2No * Math.Log(p2No, 2));
+                    if (Double.IsNaN(entropyChild)!=true)
+                    {
+                       // entropyChild = 0;
+                        childEntropyDict.Add(i, entropyChild);
+                    }
+                    int t = 1;
+                    //else childEntropyDict.Add(i, entropyChild);
+
+                    //childEntropyList.Add(entropyChild);
+
+
                 }
-                p2Yes = secondValueYesCount / secondValueLineNumbers.Count;
-                p2No = secondValueNoCount / secondValueLineNumbers.Count;
-                entropyChild = p1 * (-p1Yes * Math.Log(p1Yes, 2) - p1No * Math.Log(p1No, 2)) + p2 * (-p2Yes * Math.Log(p2Yes, 2) - p2No * Math.Log(p2No, 2));
-
-                childEntropyList.Add(entropyChild);
-
+                firstValueLineNumbers.Clear();
+                secondValueLineNumbers.Clear();
+                // вроде я не здесь пишу
+                
             }
-            double minEntropy = childEntropyList.Min();
-            int indexOfAttribute = childEntropyList.IndexOf(minEntropy);
-            usedAttributes[indexOfAttribute] = true;
-            lineNumbers.Remove(indexOfAttribute);
+            double minEntropy = childEntropyDict.Values.Min();
+            //var myKey = types.FirstOrDefault(x => x.Value == "one").Key;
+            int indexOfAttribute = childEntropyDict.FirstOrDefault(x => x.Value == minEntropy).Key; // можно в словаре поменять ключ и значение местами
+            usedAttributes[indexOfAttribute] = true; // будет ли видно из рекурсии это?
+                                                     // to do вывести в консоль по какому атрибуту делим и какие номера строк имеем после разделения
+                                                     // to do искать номера строк к одним и вторым значением в значениях выбранного атрибута
+            Console.WriteLine("Делим по атрибуту " + tableHeader[indexOfAttribute]);
+            /*List<int> firstValueNumbers = new List<int>();//можно объявить в начале  
+            List<int> secondValueNumbers = new List<int>();
+            firstValueNumbers.*/
+            for (int i = 0; i < lineNumbers.Count; i++)
+            {
+                if (tableList[lineNumbers[i]][indexOfAttribute] == attributeValuesDict[indexOfAttribute][0])
+                {
+                    firstValueLineNumbers.Add(lineNumbers[i]);
+                }
+                else secondValueLineNumbers.Add(lineNumbers[i]);
+              
+            }
+            Console.Write("Строки разделились на ");
+            for (int i = 0; i < firstValueLineNumbers.Count; i++)
+            {
+                Console.Write(firstValueLineNumbers[i].ToString() + " ");
+            }
+            Console.Write(" и ");
+            for (int i = 0; i < secondValueLineNumbers.Count; i++)
+            {
+                Console.Write(secondValueLineNumbers[i].ToString() + " ");
+            }
+            Console.WriteLine();
+
+            run(firstValueLineNumbers);
+            run(secondValueLineNumbers);
             int d = 0;
 
         }
